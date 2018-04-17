62b2c66aad47192007f1ad18af0402accffc953b
Ben Vickers <ben.vickers@opencredo.com>
2018-04-17 18:22:09 +0100

    fix image push with tag

--- release+deploy.sh
+++ release+deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -n "$TF_VAR_app_version" ]; then 
-    docker push "benjvi/terraform-cd-example-app:$1"
+    docker push "benjvi/terraform-cd-example-app:$TF_VAR_app_version"
 fi
 docker push benjvi/terraform-cd-example-app:latest
 
