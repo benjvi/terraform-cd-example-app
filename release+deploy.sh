711127345596bbf78709b677f928ab66ba9dd206
Ben Vickers <ben.vickers@opencredo.com>
2018-04-17 17:36:06 +0100

    fix app_version variable passing

--- release+deploy.sh
+++ release+deploy.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-if [ -n "$1" ]; then 
+if [ -n "$TF_VAR_app_version" ]; then 
     docker push "benjvi/terraform-cd-example-app:$1"
-    TF_ARGS="-var \"app_version=$1\""
 fi
 docker push benjvi/terraform-cd-example-app:latest
 
 cd deploy
 terraform init -upgrade
 terraform workspace select test
-terraform apply -auto-approve -input=false $TF_ARGS
+terraform apply -auto-approve -input=false
 cd -
