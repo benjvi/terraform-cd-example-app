1ea2b247c8b76b0765f5313dc8a043af85879500
Ben Vickers <ben.vickers@opencredo.com>
2018-04-17 12:37:57 +0100

    fix url and disable build for now

--- release+deploy.sh
+++ release+deploy.sh
@@ -4,8 +4,8 @@ gcloud container clusters get-credentials test-cluster --zone europe-west2-a
 cp .terraformrc "$HOME/.terraformrc"
 export GOOGLE_APPLICATION_CREDENTIALS="$HOME/API Project-20f67b6e6d86.json"
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker push benjvi/terraform-cd-example-app:${TRAVIS_BUILD_NUMBER}
-docker push benjvi/terraform-cd-example-app:latest
+#docker push benjvi/terraform-cd-example-app:${TRAVIS_BUILD_NUMBER}
+#docker push benjvi/terraform-cd-example-app:latest
 
 gcloud version
 cd deploy
