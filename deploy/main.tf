da8a7029aa53383fa695ff62e375991c0cf94f6d
Ben Vickers <ben.vickers@opencredo.com>
2018-04-17 13:12:47 +0100

    fix prod moduel to not use ssh

--- deploy/main.tf
+++ deploy/main.tf
@@ -45,7 +45,8 @@ module "app-test" {
 }
 
 module "app-prod" {
-  source = "git@github.com:benjvi/example-app-tf-module.git?ref=v1" 
+  # source = "git@github.com:benjvi/example-app-tf-module.git?ref=v1" 
+  source = "github.com/benjvi/example-app-tf-module" 
   module_count = "${terraform.workspace == "prod" ? "1" : "0"}"
   cloudflare_domain = "${var.cloudflare_domain}"
 }
