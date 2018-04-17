1f5c2c7a79c2b4f4382f06a1a88f7ac8dbaac9c6
Ben Vickers <ben.vickers@opencredo.com>
2018-04-17 13:57:42 +0100

    clean up travis / release scripts

--- release+deploy.sh
+++ release+deploy.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
-# see GH issue kubernetes/kubernetes #30617o
-gcloud container clusters get-credentials test-cluster --zone europe-west2-a
-cp .terraformrc "$HOME/.terraformrc"
-export GOOGLE_APPLICATION_CREDENTIALS="$HOME/API Project-20f67b6e6d86.json"
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-#docker push benjvi/terraform-cd-example-app:${TRAVIS_BUILD_NUMBER}
-#docker push benjvi/terraform-cd-example-app:latest
+if [ -n "$1" ]; then 
+    docker push "benjvi/terraform-cd-example-app:$1"
+fi
+docker push benjvi/terraform-cd-example-app:latest
 
-gcloud version
 cd deploy
-gcloud version
-gsutil cp gs://tf-state-benjvi/terraform/state/snap-app/default.tfstate .
-rm default.tfstate
 terraform init -upgrade
 terraform workspace select test
 terraform apply -auto-approve -input=false
