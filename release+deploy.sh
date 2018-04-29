a2ff4aa93dd0857f667b6a56cc2ea6a44eec61a3
Ben Vickers <ben.vickers@opencredo.com>
2018-04-29 13:34:56 +0100

    debug travis workspace

--- release+deploy.sh
+++ release+deploy.sh
@@ -8,12 +8,15 @@ fi
 app_version="$1"
 docker push "benjvi/terraform-cd-example-app:${app_version}"
 docker push benjvi/terraform-cd-example-app:latest
+pwd
 ./tf-pkg-build.sh "tf-state-benjvi/terraform-cd-example-app" "${app_version}" "gcs" # todo: repo type should be mandatory + part of id
 ./tf-pkg-push.sh "tf-state-benjvi/terraform-cd-example-app" "${app_version}" "gcs"
+pwd
+ls -la
 # need to push first because config refers to the module in gcs
 cd tf-pkg-tgt
+ls -la
 terraform init -upgrade
 terraform workspace select test
 terraform apply -auto-approve -input=false
 cd -
-./tf-pkg-push.sh "tfstate-benjvi/terraform-cd-example-app" "${app_version}" "gcs" 
